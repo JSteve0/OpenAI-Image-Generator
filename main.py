dfbb02d70f0e52c5b73aa344c9b519ffab4de95c
Justin <jmms5757@gmail.com>
2023-05-20 22:35:19 -0400

    Added repl and generated new images

--- main.py
+++ main.py
@@ -10,20 +10,28 @@ load_dotenv()
 openai.api_key = os.getenv('GPT_KEY')
 
 
-def download_image(url, path_to_save):
+def download_image(url: str, path_to_save: str) -> str:
     response = requests.get(url)
 
+    generated_path = path_to_save + get_date() + ".png"
+
     if response.status_code == 200:  # Check if the request was successful
-        with open(path_to_save + get_date() + ".png", 'wb') as f:
+        with open(generated_path, 'wb') as f:
             f.write(response.content)
+            f.flush()
+            os.fsync(f.fileno())
+
+    return generated_path
 
 
-def add_image_url_to_file(url: str, filename):
+def add_image_url_to_file(url: str, filename: str):
     with open(filename, 'a') as f:
         f.write(url + '\n')
+        f.flush()
+        os.fsync(f.fileno())
 
 
-def get_date():
+def get_date() -> str:
     # Get the current date
     now = datetime.now()
 
@@ -45,11 +53,27 @@ def generate_image(generation_prompt: str, number_of_images: int, size: object) 
         print(e.error)
 
 
-if __name__ == '__main__':
-    print('Generating image')
-    response_url = generate_image("art for a low poly tower defense game", 1, ImageSize.LARGE)
-    print('Downloading image')
-    download_image(response_url, 'images/')
-    add_image_url_to_file(response_url, "url.txt")
-    print('Finished')
+def repl():
+    while True:
+        print("Enter a prompt for OpenAI to generate an image for. Type Help for other usage.")
+        user_input = input("CLI: ").lower()
+        if user_input == "help":
+            print("Usage: ")
+            print("\tType exit, quit, q, e to exit")
+            input("Press Enter to continue...")
+            os.system('cls||clear')
+        elif user_input == "quit" or user_input == "q" or user_input == "exit" or user_input == "e":
+            return
+        else:
+            print('Generating image for prompt: ' + user_input)
+            response_url = generate_image(user_input, 1, ImageSize.LARGE)
+            print('Image created at link: ' + response_url)
+            print('Downloading image')
+            image_location = download_image(response_url, 'images/')
+            add_image_url_to_file(response_url, "url.txt")
+            print('Finished image can be found at: ' + image_location)
 
+
+if __name__ == '__main__':
+    repl()
+    print("Bye!")
